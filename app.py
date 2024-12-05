b52c3cb0670d3f661984812ce3e8275ae1c39afa
Gideon-Ogunbanjo <dejiogunbanjo9@gmail.com>
2024-12-05 20:48:34 +0100

    updated system prompt

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def generate_response(prompt):
     final_prompt="\n".join(history)
 
     data={
-        "model":"Brainrot AI",
+        "model":"gid30n",
         "prompt":final_prompt,
         "stream":False
     }
